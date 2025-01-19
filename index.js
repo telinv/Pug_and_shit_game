4036e72f6fb007c947e2cf3059d86cd39529a9a9
telinv <linatorgashova2002@mail.ru>
2025-01-19 23:29:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const pug = document.getElementById("pug");
+
+function jump(){
+    if(pug.classList != "jump"){
+        pug.classList.add("jump");
+
+        setTimeout(function(){
+            pug.classList.remove("jump");
+        }, 300);
+    }
+}
+
+document.addEventListener("keydown", function(event){
+    if (event.key === " "){
+        jump();
+    }
+})
+
+const shit = document.getElementById("shit");
+let isAlive = setInterval(function (){
+    let pugTop = parseInt(window.getComputedStyle(pug).getPropertyValue("top"));
+    let shitLeft = parseInt(window.getComputedStyle(shit).getPropertyValue("left"));
+    if (shitLeft < 50 && shitLeft > 0 && pugTop >= 140){
+        alert("О ноу! Поел говна");
+    }
+}, 10);
